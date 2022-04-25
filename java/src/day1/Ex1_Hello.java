98a0c1ea6ce754d7b0c78be3638459b84ba25537
Lee You Cheol <seap07@naver.com>
2022-04-25 13:55:56 +0900

    Create Ex1_Hello.java

---
+++ java/src/day1/Ex1_Hello.java
@@ -0,0 +1,21 @@
+package day1;
+
+public class Ex1_Hello {
+//콘솔 응용 프로그램이 실행되게 위해서는 main이라는 애(메소드)가 필요하다
+	
+	public static void main(String[] args) {
+		/*println(문자열) : 문자열을 콘솔에 출력한 후 줄바꿈을 함(엔터효과)
+		 * print(문자열) : 문자열을 콘솔에 출력
+		 */
+		System.out.print("Hello world!!");
+		System.out.println("Hello world!!"); //주석이다
+		System.out.print("Hello world!!");
+		/* 여
+		 * 러
+		 * 줄
+		 * 주
+		 * 석
+		 */
+	}
+
+}

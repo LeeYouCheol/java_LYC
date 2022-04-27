f049688b85245ec9e1eecdec1bb14e76a17f3a3e
Lee You Cheol <seap07@naver.com>
2022-04-27 13:59:30 +0900

    자바 3일차 : 스캐너 if 배수 예제

---
+++ java/src/day3/Ex5_if2.java
@@ -0,0 +1,22 @@
+package day3;
+
+public class Ex5_if2 {
+
+	public static void main(String[] args) {
+		/* num가 6의 배수인지 아닌지 판별하는 코드를 중첩 if문으로 작성하세요.
+		 * 
+		 * */
+		int num = 12;
+		
+		if(num % 6 == 0) {
+			System.out.println(num + "는 6의 배수");
+		}
+		else {
+			System.out.println(num + "는 6의 배수가 아님");
+		}
+		
+			
+		}
+	}
+
+}

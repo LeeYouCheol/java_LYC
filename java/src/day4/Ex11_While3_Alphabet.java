d9cda4c2eb745f93d5c62117852f94a3b0e94409
Lee You Cheol <seap07@naver.com>
2022-04-28 17:07:03 +0900

    자바 4일차 : While문 예제 - 알파벳

---
+++ java/src/day4/Ex11_While3_Alphabet.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class Ex11_While3_Alphabet {
+
+	public static void main(String[] args) {
+		/* A에서 Z까지 출력하는 코드를 while문으로 작성하세요.
+		 * */
+		char i = 'a';
+		while(i<='z') {
+			System.out.print(i);
+			i++;
+		}
+		
+	}
+
+}

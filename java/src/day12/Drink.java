83f3cfa45b37c3971e0ae6e6bfa175bf13745eb4
Lee You Cheol <seap07@naver.com>
2022-05-11 17:14:40 +0900

    자바 12일차 : 마트관리 프로그램

---
+++ java/src/day12/Drink.java
@@ -0,0 +1,30 @@
+package day12;
+
+public class Drink extends Product {
+
+	private int capacity; //용량 ml
+	
+	public Drink(String name, int price, int amount, int capacity) {
+		super(name, price, amount);
+		this.capacity = capacity;
+	}
+
+	@Override
+	public void print() {
+		if(capacity < 1000) {
+		System.out.println(getName() + "[" + capacity + "ml] : "+ getPrice() + "원 - " + getAmount());
+		}
+		else {
+			System.out.println(getName() + "[" + capacity + "L] : "+ getPrice() + "원 - " + getAmount());
+		}
+	}
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+}

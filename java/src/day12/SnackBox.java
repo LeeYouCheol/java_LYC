a4e35ebc26226482d786a0e256a25539fac507ac
Lee You Cheol <seap07@naver.com>
2022-05-12 10:21:36 +0900

    자바 12일차 : 마트관리 프로그램

--- java/src/day12/SnackBox.java
+++ java/src/day12/SnackBox.java
@@ -8,7 +8,9 @@ public class SnackBox extends Product {
 		super(name, Price, amount);
 		this.count = count;
 	}
-
+	public SnackBox(SnackBox product) {
+		this(product.getName(), product.getPrice(), product.getAmount(), product.getCount());
+	}
 	@Override
 	public void print() {
 		System.out.println(getName() + "[박스당 " + count + "개] : " + getPrice() +"원 - "+ getAmount());

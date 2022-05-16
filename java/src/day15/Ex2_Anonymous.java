6bf6627965bc2da8ee4db814b369eceb5506500f
Lee You Cheol <seap07@naver.com>
2022-05-16 17:30:10 +0900

    자바 15일차 : 익명 객체 예제

---
+++ java/src/day15/Ex2_Anonymous.java
@@ -0,0 +1,36 @@
+package day15;
+
+public class Ex2_Anonymous {
+
+	public static void main(String[] args) {
+		//구현 클래스 생성후 객체를 생성
+		Test test = new TestA(); // 얘가 객체
+		test.test();
+		
+		//구현 클래스 생성 없이 이름 없는 클래스를 바로 만들어서 객체를 생성
+		Test test2 = new Test() {
+
+			@Override
+			public void test() {
+				System.out.println("테스트2");
+			}
+			
+		};
+		test2.test();
+
+	}
+
+}
+interface Test{
+	void test();
+}
+//구현클래스 만들기
+class TestA implements Test{
+
+	@Override
+	public void test() {
+		System.out.println("테스트");
+		
+	}
+	
+}

059b984ac9751ffeeeac448f457dbfd2e5906a63
Lee You Cheol <seap07@naver.com>
2022-05-23 17:14:29 +0900

    Update Ex2_BaseballGameMain.java

--- java/src/day14/Ex2_BaseballGameMain.java
+++ java/src/day14/Ex2_BaseballGameMain.java
@@ -2,7 +2,7 @@ package day14;
 
 import java.util.Scanner;
 
-public class Ex2_BaseballGame2 {
+public class Ex2_BaseballGameMain {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);

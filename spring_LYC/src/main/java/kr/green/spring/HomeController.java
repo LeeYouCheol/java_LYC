4bb49c6d159f586361cfbaf89f7573c16ff4780f
Lee You Cheol <seap07@naver.com>
2022-08-02 12:03:19 +0900

    spring : 타일즈 최적화 완료

--- spring_LYC/src/main/java/kr/green/spring/HomeController.java
+++ spring_LYC/src/main/java/kr/green/spring/HomeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 	// http://localhost:8080/spring/main/home
-	@RequestMapping(value= {"/main/home","/home.do"})
+	@RequestMapping(value= {"/"})
 	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
 		mv.setViewName("/main/home");
 		mv.addObject("setHeader", "타일즈");

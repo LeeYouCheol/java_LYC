da094e18017c2f89900a8ff540b9467d99d0b98d
Lee You Cheol <seap07@naver.com>
2022-08-03 16:54:27 +0900

    string : DB연동 테스트

--- spring_LYC/src/main/java/kr/green/spring/serveice/MemberServiceImp.java
+++ spring_LYC/src/main/java/kr/green/spring/serveice/MemberServiceImp.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.green.spring.dao.MemberDAO;
+import kr.green.spring.vo.MemberVO;
 
 @Service
 public class MemberServiceImp implements MemberService{
@@ -12,11 +13,31 @@ public class MemberServiceImp implements MemberService{
 	MemberDAO memberDao;
 
 	@Override
-	public String getEmail(String id) {
-		if(id == null) {
+	public String getEmail(String me_id) {
+		if(me_id == null) {
 			return null;
 		}else {
-			return memberDao.selectEmail(id);
+			return memberDao.selectEmail(me_id);
 		}
+	}
+
+	@Override
+	public MemberVO getMember(String me_id) {
+		if(me_id == null)
+		return null;
+		else
+		return memberDao.selectMember(me_id);
+	}
+
+	@Override
+	public MemberVO getMember(MemberVO member) {
+		if(member == null || member.getMe_id() == null || member.getMe_pw() == null)
+		return null;
+		MemberVO dbmember = memberDao.selectMember(member.getMe_id());
+		if(dbmember == null)
+		return null;
+		if(dbmember.getMe_pw().equals(member.getMe_pw()))
+			return dbmember;
+		return null;
 	}
 }

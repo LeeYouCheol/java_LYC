2e7c5041da1181677536380ac07d6fa0e0d32231
Lee You Cheol <seap07@naver.com>
2022-08-05 12:30:16 +0900

    spring : 게시물 리스트 기능 구현 완료

--- spring_LYC/src/main/java/kr/green/spring/serveice/BoardService.java
+++ spring_LYC/src/main/java/kr/green/spring/serveice/BoardService.java
@@ -1,10 +1,14 @@
 package kr.green.spring.serveice;
 
+import java.util.ArrayList;
+
 import kr.green.spring.vo.BoardVO;
 import kr.green.spring.vo.MemberVO;
 
 public interface BoardService {
 
 	void isertBoard(BoardVO board, MemberVO user);
+
+	ArrayList<BoardVO> getBoardList();
 
 }

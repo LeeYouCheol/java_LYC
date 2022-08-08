c5a1438741d2868547c14468a16d23e90357fb8f
Lee You Cheol <seap07@naver.com>
2022-08-08 12:21:29 +0900

    spring 복습 : DB연동 및 이메일 검색과 출력

--- spring_LYC_TEST/src/main/java/kr/green/springtest/controller/HomeController.java
+++ spring_LYC_TEST/src/main/java/kr/green/springtest/controller/HomeController.java
@@ -1,23 +1,27 @@
-package kr.green.springtest;
+package kr.green.springtest.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import kr.green.springtest.service.MemberService;
 
 /**
  * Handles requests for the application home page.
  */
 @Controller
 public class HomeController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	MemberService memberService;
 	
 	@RequestMapping(value= "/")
-	public ModelAndView openTilesView(ModelAndView mv) throws Exception{
+	public ModelAndView openTilesView(ModelAndView mv) {
 		mv.setViewName("/main/home");
 		mv.addObject("setHeader", "타일즈");
+		System.out.println(memberService.getEmail("a"));
 		return mv;
 	}
 }

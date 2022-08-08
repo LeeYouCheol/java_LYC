1e65a24bade3e4ab68d5273ee515dee418f08fdf
Lee You Cheol <seap07@naver.com>
2022-08-08 15:21:36 +0900

    spring 복습 : 회원가입 기능 완료

--- spring_LYC_TEST/src/main/java/kr/green/springtest/controller/HomeController.java
+++ spring_LYC_TEST/src/main/java/kr/green/springtest/controller/HomeController.java
@@ -5,9 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import kr.green.springtest.service.MemberService;
+import kr.green.springtest.vo.MemberVO;
 
 /**
  * Handles requests for the application home page.
@@ -17,11 +19,27 @@ public class HomeController {
 	@Autowired
 	MemberService memberService;
 	
+	//이메일 출력하는 메소드
 	@RequestMapping(value= "/")
 	public ModelAndView openTilesView(ModelAndView mv) {
 		mv.setViewName("/main/home");
 		mv.addObject("setHeader", "타일즈");
 		System.out.println(memberService.getEmail("a"));
+		return mv;
+	}
+	//회원가입하는 메소드
+	@RequestMapping(value= "/signup", method=RequestMethod.GET)
+	public ModelAndView signupGet(ModelAndView mv) {
+		mv.setViewName("/main/signup");
+		return mv;
+	}
+	@RequestMapping(value= "/signup", method=RequestMethod.POST)
+	public ModelAndView signupPost(ModelAndView mv, MemberVO member) {
+		if(memberService.signup(member)) {
+			mv.setViewName("redirect:/");
+		}else {
+			mv.setViewName("redirect:/signup");
+		}
 		return mv;
 	}
 }

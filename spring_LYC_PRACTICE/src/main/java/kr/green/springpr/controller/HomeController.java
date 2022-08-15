4f9fe4e45ca3b878e38d931fcc38f50beae4aeab
Lee You Cheol <seap07@naver.com>
2022-08-15 16:32:10 +0900

    spring 연습 : DB연동

--- spring_LYC_PRACTICE/src/main/java/kr/green/springpr/controller/HomeController.java
+++ spring_LYC_PRACTICE/src/main/java/kr/green/springpr/controller/HomeController.java
@@ -2,22 +2,26 @@ package kr.green.springpr.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Handles requests for the application home page.
- */
+import kr.green.springpr.service.MemberService;
+
+
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	@RequestMapping(value = "/main/home")
-	public ModelAndView openTilesView(ModelAndView mv) {
-		mv.setViewName("/main/home");
-		mv.addObject("setHeader", "타일즈");
-		return mv;
-	}
-	
+	@Autowired
+    MemberService memberService;
+    
+    @RequestMapping(value="/")
+    public ModelAndView main(ModelAndView mv) throws Exception{
+        mv.setViewName("/main/home");
+        mv.addObject("setHeader", "타일즈테스트");
+        System.out.println(memberService.getEmail("qwe123"));
+        return mv;
+    }
+
 }
---
+++ spring_LYC_PRACTICE/src/main/java/kr/green/springpr/service/MemberService.java
@@ -0,0 +1,5 @@
+package kr.green.springpr.service;
+
+public interface MemberService {
+    public String getEmail(String id);
+}
